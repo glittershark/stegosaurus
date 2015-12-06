bdfb7e5bca67d7457f88a8ff0bb93f8513a02ad0
Griffin Smith <wildgriffin45@gmail.com>
2015-12-06 16:45:12 -0500

    Slightly improve CLI for birth date of person

--- Main.hs
+++ Main.hs
@@ -5,6 +5,15 @@ import Prelude
 import Web.Stegosaurus.DBPedia
 import Data.Hourglass.Types (Date)
 
+person :: IO Text
+person = do
+    args <- getArgs
+    return $ fromMaybe deflt $ headMay args
+    where deflt = "Benjamin_Franklin"
+
 main :: IO ()
-main = (birthDateOf "Benjamin_Franklin" :: IO (Either SomeException Date))
-       >>= print
+main = do
+    bd <- birthDateOf =<< person :: IO (Either SomeException Date)
+    case bd of
+      Left ex -> putStrLn $ "Could not find birth date: " ++ tshow ex
+      Right d -> print d
